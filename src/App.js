af613d7ee26031807662d8ae2b050aa32e258cf6
AxLSTU <leha2148@gmail.com>
2020-11-15 14:34:00 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Switch from "./components/Switch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>toggle button</h2>
+      <Switch />
+    </div>
+  );
+}
---
+++ src/components/Switch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Switch = () => {
+  return <div>I am Switch</div>;
+};
+
+export default Switch;
