8ee48e83f6be49f4c3ffb298dae7be3a2f861b76
AxLSTU <leha2148@gmail.com>
2020-11-15 15:16:59 +0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Switch from "./components/Switch";
 
 export default function App() {
+  const [isToggled, setIsToggled] = useState(false);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>toggle button</h2>
-      <Switch />
+      <Switch
+        roundet={true}
+        isToggled={isToggled}
+        onToggle={() => setIsToggled(!isToggled)}
+      />
     </div>
   );
 }
--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Switch.css";
+import cx from "classnames";
 
-const Switch = () => {
+const Switch = ({ roundet = false, isToggled, onToggle }) => {
+  const sliderCX = cx("switch-slider", {
+    roundet: roundet
+  });
+
   return (
     <label className="switch-label">
-      <input type="checkbox" />
-      <span className="switch-slider" />
+      <input type="checkbox" checked={isToggled} onChange={onToggle} />
+      <span className={sliderCX} />
     </label>
   );
 };
